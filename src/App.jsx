651f723f95f9ed7531391562d804918afdc76497
jonayet-nur <jonayetnur@gmail.com>
2026-03-30 10:24:45 +0600

    project enviroment setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2 className='text-2xl text-red-700 bg-blue-300'>Hello world!</h2>
+     <button className="btn btn-neutral">Neutral</button>
+    </>
+  )
+}
+
+export default App
